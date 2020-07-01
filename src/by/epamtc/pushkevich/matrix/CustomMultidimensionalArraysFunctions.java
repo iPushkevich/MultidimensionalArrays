0d10d7edf8c7e99249a2d25ed4580a34a8df8d0f
Igor Pushkevich <igorogi@bk.ru>
2020-07-01 16:57:25 +0300

    renamed parameters

--- src/by/epamtc/pushkevich/matrix/CustomMultidimensionalArraysFunctions.java
+++ src/by/epamtc/pushkevich/matrix/CustomMultidimensionalArraysFunctions.java
@@ -3,38 +3,32 @@ package by.epamtc.pushkevich.matrix;
 public class CustomMultidimensionalArraysFunctions {
     public static void main(String[] args) {
 
-        int a[][] = getMagicSquare(3);
-
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                System.out.print(a[i][j] + " ");
-            }
-            System.out.println();
-        }
     }
 
-    private static int[][] getMatrixByExample1(int size) {
-        int matrix[][] = new int[size][size];
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+    private static int[][] getMatrixByExample1(int evenSize) {
+        int matrix[][] = new int[evenSize][evenSize];
+
+        for (int i = 0; i < evenSize; i++) {
+            for (int j = 0; j < evenSize; j++) {
                 if (i % 2 == 0) {
                     matrix[i][j] = j + 1;
-                } else {
-                    matrix[i][j] = size - j;
+                }
+                else {
+                    matrix[i][j] = evenSize - j;
                 }
             }
         }
         return matrix;
     }
 
-    private static int[][] getMatrixByExample2(int size) {
-        int[][] matrix = new int[size][size];
+    private static int[][] getMatrixByExample2(int evenSize) {
+        int[][] matrix = new int[evenSize][evenSize];
         int firstNumber = 1;
         int secondNumber = 2;
 
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
+        for (int i = 0; i < evenSize; i++) {
+            for (int j = 0; j < evenSize; j++) {
                 if (i == j) {
                     matrix[i][j] = firstNumber * secondNumber;
                     firstNumber++;
@@ -46,19 +40,20 @@ public class CustomMultidimensionalArraysFunctions {
         return matrix;
     }
 
-    private static int[][] getMatrixByExample3(int size) {
-        int[][] matrix = new int[size][size];
+    private static int[][] getMatrixByExample3(int evenSize) {
+        int[][] matrix = new int[evenSize][evenSize];
 
-        for (int i = 0; i < size / 2; i++) {
-            for (int j = i; j < size - i; j++) {
+        for (int i = 0; i < evenSize / 2; i++) {
+            for (int j = i; j < evenSize - i; j++) {
                 matrix[i][j] = 1;
-                matrix[size - i - 1][size - j - 1] = 1;
+                matrix[evenSize - i - 1][evenSize - j - 1] = 1;
             }
         }
+
         return matrix;
     }
 
-    private static long[][] getMatrixOfArray(int[] array) {
+    private static long[][] getMatrixFromArray(int[] array) {
         int size = array.length;
         long[][] matrix = new long[size][size];
 
@@ -87,11 +82,11 @@ public class CustomMultidimensionalArraysFunctions {
 
     private static int[][] getMagicSquare(int size) {
         int[][] magicSquare = new int[size][size];
-        int row = size-1;
-        int col = size/2;
+        int row = size - 1;
+        int col = size / 2;
         magicSquare[row][col] = 1;
 
-        for (int i = 2; i <= size*size; i++) {
+        for (int i = 2; i <= size * size; i++) {
             if (magicSquare[(row + 1) % size][(col + 1) % size] == 0) {
                 row = (row + 1) % size;
                 col = (col + 1) % size;
